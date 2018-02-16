f425bfb83100ad01523c4ef0aa1994be2415f731
Pat Guettler <patguettler@gmail.com>
2018-02-16 15:32:49 -0600

    Me Files

---
+++ NetworkListener.c
@@ -0,0 +1,299 @@
+//C/C++ 
+//Stream network data by binding to a port (through a config or command line option)
+//Store data into an object like a char[]
+//Write it back to the network
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <stdio.h> //For standard things
+#include <stdlib.h>    //malloc
+#include <string.h>    //memset
+#include <netinet/ip_icmp.h>   //Provides declarations for icmp header
+#include <netinet/udp.h>   //Provides declarations for udp header
+#include <netinet/tcp.h>   //Provides declarations for tcp header
+#include <netinet/ip.h>    //Provides declarations for ip header
+#include <sys/socket.h>
+#include <arpa/inet.h>
+ 
+void ProcessPacket(unsigned char* , int);
+void print_ip_header(unsigned char* , int);
+void print_tcp_packet(unsigned char* , int);
+void print_udp_packet(unsigned char * , int);
+void print_icmp_packet(unsigned char* , int);
+void PrintData (unsigned char* , int);
+int ProcessArgsAndGetPort(int argc, char** argv);
+ 
+int sock_raw;
+FILE *logfile;
+int tcp=0,udp=0,icmp=0,others=0,igmp=0,total=0,i,j;
+struct sockaddr_in source,dest;
+
+int main(int argc, char** argv) 
+{
+    int port = ProcessArgsAndGetPort(argc, argv);
+
+    if(port == 0){
+      printf("Please enter a port\n");
+      return 0; 
+    }
+
+    printf("Your port number is: %d\n", port);
+
+    int saddr_size , data_size;
+    struct sockaddr saddr;
+    struct in_addr in;
+     
+    unsigned char *buffer = (unsigned char *)malloc(65536); //Its Big!
+     
+    logfile=fopen("log.txt","w");
+    if(logfile==NULL) printf("Unable to create file.");
+    printf("Starting...\n");
+    //Create a raw socket that shall sniff
+    sock_raw = socket(AF_INET , SOCK_RAW , IPPROTO_TCP);
+    if(sock_raw < 0)
+    {
+        printf("Socket Error\n");
+        return 1;
+    }
+    while(1)
+    {
+        saddr_size = sizeof saddr;
+        //Receive a packet
+        data_size = recvfrom(sock_raw , buffer , 65536 , 0 , &saddr , &saddr_size);
+        if(data_size <0 )
+        {
+            printf("Recvfrom error , failed to get packets\n");
+            return 1;
+        }
+        //Now process the packet
+       // ProcessPacket(buffer , data_size);
+       printf("Packet found\n");
+    }
+
+    return 0;
+}
+ 
+void ProcessPacket(unsigned char* buffer, int size)
+{
+    //Get the IP Header part of this packet
+    struct iphdr *iph = (struct iphdr*)buffer;
+    ++total;
+    switch (iph->protocol) //Check the Protocol and do accordingly...
+    {
+        case 6:  //TCP Protocol
+            ++tcp;
+            print_tcp_packet(buffer , size);
+            break;
+         
+        case 17: //UDP Protocol
+            ++udp;
+            print_udp_packet(buffer , size);
+            break;
+         
+        default: //Some Other Protocol like ARP etc.
+            ++others;
+            break;
+    }
+    printf("TCP : %d   UDP : %d Others : %d   Total : %d\r",tcp,udp,others,total);
+}
+
+void print_ip_header(unsigned char* Buffer, int Size)
+{
+    unsigned short iphdrlen;
+     
+    struct iphdr *iph = (struct iphdr *)Buffer;
+    iphdrlen = iph->ihl*4;
+     
+    struct tcphdr *tcph=(struct tcphdr*)(Buffer + iphdrlen);
+             
+    fprintf(logfile,"\n\n***********************TCP Packet*************************\n");    
+         
+    print_ip_header(Buffer,Size);
+         
+    fprintf(logfile,"\n");
+    fprintf(logfile,"TCP Header\n");
+    fprintf(logfile,"   |-Source Port      : %u\n",ntohs(tcph->source));
+    fprintf(logfile,"   |-Destination Port : %u\n",ntohs(tcph->dest));
+    fprintf(logfile,"   |-Sequence Number    : %u\n",ntohl(tcph->seq));
+    fprintf(logfile,"   |-Acknowledge Number : %u\n",ntohl(tcph->ack_seq));
+    fprintf(logfile,"   |-Header Length      : %d DWORDS or %d BYTES\n" ,(unsigned int)tcph->doff,(unsigned int)tcph->doff*4);
+    //fprintf(logfile,"   |-CWR Flag : %d\n",(unsigned int)tcph->cwr);
+    //fprintf(logfile,"   |-ECN Flag : %d\n",(unsigned int)tcph->ece);
+    fprintf(logfile,"   |-Urgent Flag          : %d\n",(unsigned int)tcph->urg);
+    fprintf(logfile,"   |-Acknowledgement Flag : %d\n",(unsigned int)tcph->ack);
+    fprintf(logfile,"   |-Push Flag            : %d\n",(unsigned int)tcph->psh);
+    fprintf(logfile,"   |-Reset Flag           : %d\n",(unsigned int)tcph->rst);
+    fprintf(logfile,"   |-Synchronise Flag     : %d\n",(unsigned int)tcph->syn);
+    fprintf(logfile,"   |-Finish Flag          : %d\n",(unsigned int)tcph->fin);
+    fprintf(logfile,"   |-Window         : %d\n",ntohs(tcph->window));
+    fprintf(logfile,"   |-Checksum       : %d\n",ntohs(tcph->check));
+    fprintf(logfile,"   |-Urgent Pointer : %d\n",tcph->urg_ptr);
+    fprintf(logfile,"\n");
+    fprintf(logfile,"                        DATA Dump                         ");
+    fprintf(logfile,"\n");
+         
+    fprintf(logfile,"IP Header\n");
+    PrintData(Buffer,iphdrlen);
+         
+    fprintf(logfile,"TCP Header\n");
+    PrintData(Buffer+iphdrlen,tcph->doff*4);
+         
+    fprintf(logfile,"Data Payload\n");  
+    PrintData(Buffer + iphdrlen + tcph->doff*4 , (Size - tcph->doff*4-iph->ihl*4) );
+                         
+    fprintf(logfile,"\n###########################################################");
+}
+
+void print_tcp_packet(unsigned char* Buffer, int Size)
+{
+    unsigned short iphdrlen;
+     
+    struct iphdr *iph = (struct iphdr *)Buffer;
+    iphdrlen = iph->ihl*4;
+     
+    struct tcphdr *tcph=(struct tcphdr*)(Buffer + iphdrlen);
+             
+    fprintf(logfile,"\n\n***********************TCP Packet*************************\n");    
+         
+    print_ip_header(Buffer,Size);
+         
+    fprintf(logfile,"\n");
+    fprintf(logfile,"TCP Header\n");
+    fprintf(logfile,"   |-Source Port      : %u\n",ntohs(tcph->source));
+    fprintf(logfile,"   |-Destination Port : %u\n",ntohs(tcph->dest));
+    fprintf(logfile,"   |-Sequence Number    : %u\n",ntohl(tcph->seq));
+    fprintf(logfile,"   |-Acknowledge Number : %u\n",ntohl(tcph->ack_seq));
+    fprintf(logfile,"   |-Header Length      : %d DWORDS or %d BYTES\n" ,(unsigned int)tcph->doff,(unsigned int)tcph->doff*4);
+    //fprintf(logfile,"   |-CWR Flag : %d\n",(unsigned int)tcph->cwr);
+    //fprintf(logfile,"   |-ECN Flag : %d\n",(unsigned int)tcph->ece);
+    fprintf(logfile,"   |-Urgent Flag          : %d\n",(unsigned int)tcph->urg);
+    fprintf(logfile,"   |-Acknowledgement Flag : %d\n",(unsigned int)tcph->ack);
+    fprintf(logfile,"   |-Push Flag            : %d\n",(unsigned int)tcph->psh);
+    fprintf(logfile,"   |-Reset Flag           : %d\n",(unsigned int)tcph->rst);
+    fprintf(logfile,"   |-Synchronise Flag     : %d\n",(unsigned int)tcph->syn);
+    fprintf(logfile,"   |-Finish Flag          : %d\n",(unsigned int)tcph->fin);
+    fprintf(logfile,"   |-Window         : %d\n",ntohs(tcph->window));
+    fprintf(logfile,"   |-Checksum       : %d\n",ntohs(tcph->check));
+    fprintf(logfile,"   |-Urgent Pointer : %d\n",tcph->urg_ptr);
+    fprintf(logfile,"\n");
+    fprintf(logfile,"                        DATA Dump                         ");
+    fprintf(logfile,"\n");
+         
+    fprintf(logfile,"IP Header\n");
+    PrintData(Buffer,iphdrlen);
+         
+    fprintf(logfile,"TCP Header\n");
+    PrintData(Buffer+iphdrlen,tcph->doff*4);
+         
+    fprintf(logfile,"Data Payload\n");  
+    PrintData(Buffer + iphdrlen + tcph->doff*4 , (Size - tcph->doff*4-iph->ihl*4) );
+                         
+    fprintf(logfile,"\n###########################################################");
+}
+ 
+void print_udp_packet(unsigned char *Buffer , int Size)
+{
+     
+    unsigned short iphdrlen;
+     
+    struct iphdr *iph = (struct iphdr *)Buffer;
+    iphdrlen = iph->ihl*4;
+     
+    struct udphdr *udph = (struct udphdr*)(Buffer + iphdrlen);
+     
+    fprintf(logfile,"\n\n***********************UDP Packet*************************\n");
+     
+    print_ip_header(Buffer,Size);           
+     
+    fprintf(logfile,"\nUDP Header\n");
+    fprintf(logfile,"   |-Source Port      : %d\n" , ntohs(udph->source));
+    fprintf(logfile,"   |-Destination Port : %d\n" , ntohs(udph->dest));
+    fprintf(logfile,"   |-UDP Length       : %d\n" , ntohs(udph->len));
+    fprintf(logfile,"   |-UDP Checksum     : %d\n" , ntohs(udph->check));
+     
+    fprintf(logfile,"\n");
+    fprintf(logfile,"IP Header\n");
+    PrintData(Buffer , iphdrlen);
+         
+    fprintf(logfile,"UDP Header\n");
+    PrintData(Buffer+iphdrlen , sizeof udph);
+         
+    fprintf(logfile,"Data Payload\n");  
+    PrintData(Buffer + iphdrlen + sizeof udph ,( Size - sizeof udph - iph->ihl * 4 ));
+     
+    fprintf(logfile,"\n###########################################################");
+}
+void PrintData (unsigned char* data , int Size)
+{
+     
+    for(i=0 ; i < Size ; i++)
+    {
+        if( i!=0 && i%16==0)   //if one line of hex printing is complete...
+        {
+            fprintf(logfile,"         ");
+            for(j=i-16 ; j<i ; j++)
+            {
+                if(data[j]>=32 && data[j]<=128)
+                    fprintf(logfile,"%c",(unsigned char)data[j]); //if its a number or alphabet
+                 
+                else fprintf(logfile,"."); //otherwise print a dot
+            }
+            fprintf(logfile,"\n");
+        } 
+         
+        if(i%16==0) fprintf(logfile,"   ");
+            fprintf(logfile," %02X",(unsigned int)data[i]);
+                 
+        if( i==Size-1)  //print the last spaces
+        {
+            for(j=0;j<15-i%16;j++) fprintf(logfile,"   "); //extra spaces
+             
+            fprintf(logfile,"         ");
+             
+            for(j=i-i%16 ; j<=i ; j++)
+            {
+                if(data[j]>=32 && data[j]<=128) fprintf(logfile,"%c",(unsigned char)data[j]);
+                else fprintf(logfile,".");
+            }
+            fprintf(logfile,"\n");
+        }
+    }
+}
+
+int ProcessArgsAndGetPort(int argc, char **argv)
+{
+   int foundPort = 0;
+   int portNumber;
+
+    //look throught the args to find port flag
+    //assume that if -p or port is found that the next item in the arg list will be the port number
+    for(int i = 0; i < argc; i++)//This can pulled out and put into a arg parser class/method
+    {
+      if(strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "-P") == 0 ){
+        
+        if(strcmp(argv[i+1], "") != 0){
+          foundPort = 1;
+          portNumber = atoi(argv[i+1]);
+        }
+
+      }	//end if
+
+      if(strcmp(argv[i], "--port") == 0 || strcmp(argv[i], "--Port") == 0){
+        
+        if(strcmp(argv[i+1], "") != 0){
+	  foundPort = 1;
+          portNumber = atoi(argv[i+1]);
+	}
+
+      }//end if	 				
+
+    }//end for
+
+    if(foundPort == 0){
+      printf("No port was found\n");
+      return 0;
+    }//end port condition	
+
+    return portNumber;
+}   
